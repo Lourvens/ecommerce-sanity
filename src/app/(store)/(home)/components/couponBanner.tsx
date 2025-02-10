3d1de0766f1778ae90709adae638a6c8b9060eff
lourvens <lourvens.luxamar@gmail.com>
2025-02-10 12:25:20 -0500

    Update Coupon Banner color

--- src/app/(store)/(home)/components/couponBanner.tsx
+++ src/app/(store)/(home)/components/couponBanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CouponBanner = () => {
   return (
-    <div className="container bg-gradient-to-r from-slate-500 to-neutral-900 text-white p-8 rounded-lg shadow-lg  mx-auto mt-3 mb-16">
+    <div className="container bg-gradient-to-r from-purple-500 to-blue-900 text-white p-8 rounded-lg shadow-lg  mx-auto mt-3 mb-16">
       <div className="text-3xl font-bold mb-4">Spesyal Piyay!</div>
       <div className="text-lg mb-4">
         Jwenn <span className="text-yellow-400 font-bold">25%</span> Rab&egrave;
@@ -11,7 +11,7 @@ const CouponBanner = () => {
       <div className="text-base mb-4">Itilize k&ograve;d coupon sa:</div>
       <div className="bg-white text-gray-800 rounded-lg px-4 py-2 flex items-center justify-between">
         <span className="text-2xl font-semibold">PIYAY25</span>
-        <button className="bg-neutral-800 text-white px-3 py-1 rounded hover:bg-neutral-600 focus:outline-none focus:ring-2 focus:ring-neutral-500">
+        <button className="bg-primary text-white px-3 py-1 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
           Copy
         </button>
       </div>
